5db3303667d7d6121bb72f29a03f7735972e598d
Jackie Wong <wongzjackie@gmail.com>
2022-11-04 00:57:31 -1000

    Add keyframes example.
    -Update styling of body.
    -Add keyframes example of square.

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../App.css';
-import { motion } from 'framer-motion';
+import { m, motion } from 'framer-motion';
 
 const Card = () => {
   return (
@@ -14,16 +14,15 @@ const Card = () => {
       <motion.div
         className='card'
         animate={{
-          scale: [1, 2, 0.5, 1.5, 1],
-          rotate: [0, 0, 270, 270, 0],
-          borderRadius: ['20%', '80%', '50%', '40%', '20%'],
+          scale: [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2],
+          rotate: [0, 270, 130, 60, 270, 200, 250],
+          borderRadius: ['30%', '30%', '30%', '30%', '30%', '30%', '30%'],
         }}
         transition={{
           duration: 2,
-          ease: 'easeOut',
-          time: [0, 0.2, 0.5, 0.8, 1],
+          ease: 'easeInOut',
+          time: [0, 0.2, 0.4, 0.5, 0.7, 0.8, 1],
           repeat: Infinity,
-          repeatDelay: 0.5,
         }}
       ></motion.div>
     </div>
