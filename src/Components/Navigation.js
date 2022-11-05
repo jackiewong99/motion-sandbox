557bad236733a0753509d5ff31d871c7f0a394f1
Jackie Wong <wongzjackie@gmail.com>
2022-11-04 20:28:04 -1000

    Fix syntax error.
    -Enforce camel-case for "staggerChildren" in transition property of Navigation's animation variants to fix syntax error.

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -4,7 +4,7 @@ import NavItem from './NavItem';
 
 const variants = {
   open: {
-    transition: { staggerchildren: 0.07, delayChildren: 0.2 },
+    transition: { staggerChildren: 0.07, delayChildren: 0.2 },
   },
   closed: {
     transition: { staggerChildren: 0.05, staggerDirection: -1 },
