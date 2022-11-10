79cf5ac66412c752492c2f9829fa67e001d1057f
Jackie Wong <wongzjackie@gmail.com>
2022-11-10 00:05:41 -1000

    Update Card.js
    -Add card container.
    -Add card header and card content with sample text.

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,9 +3,15 @@ import { motion } from 'framer-motion';
 
 const Card = () => {
   return (
-    <div>
-      <div></div>
-    </div>
+    <motion.div className='card-container'>
+      <motion.h3 className='card-header'>Card name</motion.h3>
+      <motion.p className='card-content'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque
+        voluptatem, veritatis labore sed unde suscipit. Nemo in doloribus
+        delectus ullam veniam. Ullam odio enim recusandae inventore accusantium
+        architecto, eos possimus?
+      </motion.p>
+    </motion.div>
   );
 };
 
