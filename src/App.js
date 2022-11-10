ae07b91d790f42bb519e7ca8dc9d436eca45848e
Jackie Wong <wongzjackie@gmail.com>
2022-11-10 00:06:20 -1000

    Update App.js
    -Add comments to file import statements.
    -Add list of cards.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+// Import components
 import Header from './Components/Header';
+import Card from './Components/Card';
+// Import stylesheet
 import './App.css';
 
 const App = () => {
   return (
     <div className='App'>
       <Header />
+      <div className='cards'>
+        <ul>
+          <Card />
+          <Card />
+          <Card />
+        </ul>
+      </div>
     </div>
   );
 };
